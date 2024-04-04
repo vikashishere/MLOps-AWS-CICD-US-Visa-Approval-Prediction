2cf980a1dacc7d6927371118234981b2dedea141
Vikash <vikashdas770@gmail.com>
2024-04-04 11:32:54 +0530

    exception module added

--- demo.py
+++ demo.py
@@ -1,4 +1,16 @@
 # below code is to check the logging config
-from us_visa.logger import logging
+# from us_visa.logger import logging
 
-logging.info("This is to test the logging config")
+# logging.info("This is to test the logging config")
+
+
+# below code is to check the exception config
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys) from e
