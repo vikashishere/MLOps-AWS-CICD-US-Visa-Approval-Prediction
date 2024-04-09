d5865813191fe7454d08fcdf45dacd93b2977d6a
Vikash <vikashdas770@gmail.com>
2024-04-09 11:54:24 +0530

    model eval and model pusher added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -21,6 +21,12 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -58,7 +64,7 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_BUCKET_NAME = "usvisa-model-mlopsproj"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
