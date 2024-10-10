a9d8b235ad44b751e8668042aed87f0fbad0d6bf
Vikash <vikashdas770@gmail.com>
2024-10-10 11:36:31 +0530

    updated

--- demo.py
+++ demo.py
@@ -18,7 +18,7 @@
 
 # --------------------------------------------------------------------------------
 
-from us_visa.pipline.training_pipeline import TrainPipeline
+# from us_visa.pipline.training_pipeline import TrainPipeline
 
-pipline = TrainPipeline()
-pipline.run_pipeline()
+# pipline = TrainPipeline()
+# pipline.run_pipeline()
--- us_visa/components/model_trainer.py
+++ us_visa/components/model_trainer.py
@@ -6,7 +6,7 @@ import pandas as pd
 from pandas import DataFrame
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from neuro_mf  import ModelFactory
+from neuro_mf import ModelFactory
 
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
